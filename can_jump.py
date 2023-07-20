c22dcea8238ed357d06771f6b3762347ac3c14ee
Amira Johnson <57566255+amirajohnson@users.noreply.github.com>
2023-07-20 10:32:29 -0400

    Create can_jump.py

---
+++ can_jump.py
@@ -0,0 +1,29 @@
+'''
+Prompt:
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+'''
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal, current = 0, len(nums)-1
+        check = current - 1
+        while check >= goal:
+            if nums[check] >= current-check:
+                current -= current-check #1
+                check -= 1
+            else:
+                check -= 1
+        
+        return current <= goal
